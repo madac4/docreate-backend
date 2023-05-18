d90868d6d6c4eafd6f8143f90a7a42daa334c7bc
Petru Orbu <whhroo@Petrus-MBP.Dlink>
2023-05-18 22:31:42 +0300

    Code Refactoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
