2baecb3efb65330e923d40b43e97371581e13679
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2023-05-25 16:53:08 +0300

    test

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({
-            body: 'SUKA DAAA',
-        });
-    }
+    // if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+        body: 'SUKA DAAA',
+    });
+    // }
 });
