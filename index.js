d49519efc97b195036e60cf93d82340530191c50
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2023-05-25 17:00:43 +0300

    test

--- index.js
+++ index.js
@@ -12,28 +12,20 @@ const organizationRoute = require('./routes/organizations');
 const port = 3002;
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: 'https://docreate.vercel.app/' }));
 
-// mongoose.set('strictQuery', false);
-// mongoose
-//     .connect(process.env.MONGODB_URI)
-//     .then(() => console.log('Database connect successfull'))
-//     .catch((err) => console.log(err));
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('Database connect successfull'))
+    .catch((err) => console.log(err));
 
-// app.use('/api/auth', authRoute);
-// app.use('/api/users', userRoute);
-// app.use('/api/documents', documentRoute);
-// app.use('/api/organizations', organizationRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/documents', documentRoute);
+app.use('/api/organizations', organizationRoute);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-    // if (req.method === 'OPTIONS') {
-    return res.status(200).json({
-        body: 'SUKA DAAA',
-    });
-    // }
+    res.send('Hello World!');
 });
