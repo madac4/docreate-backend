0dac116264a9031484fd95c326b630f5306b6d71
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2023-09-29 08:46:04 +0300

    cors

--- index.js
+++ index.js
@@ -11,14 +11,30 @@ const organizationRoute = require('./routes/organizations');
 
 const port = 3002;
 const app = express();
+const allowedOrigins = ['http://localhost:3000', 'https://docreate.vercel.app'];
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://docreate.vercel.app' }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  }),
+);
 
 mongoose.set('strictQuery', false);
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log('Database connect successfull'))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Database connect successfull'))
+  .catch((err) => console.log(err));
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
